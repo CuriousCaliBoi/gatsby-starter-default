fc5c943827291a36ec33d7f9360ea070b9f6f560
KingZuko <rs6121@nyu.edu>
2022-12-05 13:41:29 -0500

    added about file and modified index.js

---
+++ src/pages/about.js
@@ -0,0 +1,31 @@
+// import * as React from 'react'
+
+
+// const AboutPage = () => {
+//     return (
+//         <main>
+//             <h1> About Me</h1>
+//             <p>Hi my names Rohan and I'm communicating with you through the internet which is neat.
+//                  Hope you like the landing page I've worked on</p>
+
+//         </main>
+//     )
+// }
+
+// export const Head = () => <title><h1>About Me</h1></title>
+
+// export default AboutPage
+import * as React from 'react'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
+}
+
+export const Head = () => <title>About Me</title>
+
+export default AboutPage
