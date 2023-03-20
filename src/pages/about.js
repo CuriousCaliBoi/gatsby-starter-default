5c0153573c614d3d575bcc827eab73408443df62
KingZuko <rs6121@nyu.edu>
2023-03-19 20:48:24 -0400

    attempt at fixing about page

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+return (
+  <main>
+    <h1>About me</h1>
+    <p>Just a guy trying to get this thing up and running</p>
+
+  </main>
+)
+}
+
+// export const Head = () => <title>rohan's blog</title>
+
+export default AboutPage
